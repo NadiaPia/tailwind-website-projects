0948d4cb7099ac8fc01cb3dc6e3af1d1d6fac8b7
NadiaPia <206i417@gmail.com>
2023-11-06 21:04:41 -0800

    Finish Shortly Project

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,35 +1,10 @@
-const input = document.getElementById('link-input');
-const linkForm = document.getElementById('link-form');
-const errMsg = document.getElementById('err-msg');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 
-linkForm.addEventListener('submit', formSubmit)
+btn.addEventListener('click', navToggle);
 
-// Validate a URL
-function validURL(str) {
-    var pattern = new RegExp(
-      '^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-        '((\\d{1,3}\\.){3}\\d{1,3}))' +
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-        '(\\?[;&a-z\\d%_.~+=-]*)?' +
-        '(\\#[-a-z\\d_]*)?$',
-      'i'
-    )
-    return !!pattern.test(str)
-  }
-
-function formSubmit(e) {
-    e.preventDefault()
-
-    if(input.value === '') {
-        errMsg.innerHTML = 'Please enter something';
-        input.classList.add('border-red') //classList. boject has a method add that allows to add an attribute to the class
-    } else if (!validURL(input.value)) {
-        errMsg.innerHTML = 'Please enter a valid URL';
-        input.classList.add('border-red')
-    } else {
-        errMsg.innerHTML = '';
-        input.classList.remove('border-red')
-        alert('Success')
-    }
+function navToggle() {
+    btn.classList.toggle('open'); //turns hamburger menu view to the x-button
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden')
 }
