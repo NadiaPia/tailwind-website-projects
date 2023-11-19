adce349987b2637990dc29b02756d9f15d972201
NadiaPia <206i417@gmail.com>
2023-11-18 23:59:30 -0800

    Add onTabClick function

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -2,4 +2,17 @@ const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel')
 
 
-console.log("tabs", tabs)
+//Tabs menu event listener
+
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+function onTabClick(e) {
+    tabs.forEach((tab) => {
+        tab.children[0].classList.remove(
+            'border-softRed',
+            'border-b-4',
+            'md:border-b-0'
+
+        )
+    })
+}
